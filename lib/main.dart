3ec31e8a435769d9e2d51b917c3582ddebb5507a
amrilhakimsihotang <amrilhakimsihotang@outlook.com>
2022-02-21 14:48:19 +0700

    added Icon

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // #docregion build
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -16,45 +15,46 @@ class MyApp extends StatelessWidget {
       home: RandomWords(),
     );
   }
-
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _saved = <WordPair>{};
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
       ),
-    
       body: ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider(); 
+          if (i.isOdd) return const Divider();
 
-          final index = i ~/ 2; 
+          final index = i ~/ 2;
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
-         
+
+          final alreadySaved = _saved.contains(_suggestions[index]);
+
           return ListTile(
             title: Text(
               _suggestions[index].asPascalCase,
               style: _biggerFont,
             ),
+            trailing: Icon(alreadySaved ? Icons.favorite: Icons.favorite_border,
+            color: alreadySaved ? Colors.red: null,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+            ),
           );
-          
         },
       ),
-    
-    );
+);
   }
-  
 }
-
 
 class RandomWords extends StatefulWidget {
   const RandomWords({Key? key}) : super(key: key);
